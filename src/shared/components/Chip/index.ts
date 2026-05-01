9e86a03ec6146b0981ad311cd18a406d75ec40cf
JiHo00 <cale.jiho@gmail.com>
2026-05-01 14:08:11 +0900

    chore : src/shared/components 디렉토리 제거

--- src/shared/components/Chip/index.ts
+++
@@ -1,2 +0,0 @@
-export { Chip } from './Chip';
-export type { ChipProps, ChipColorScheme } from './Chip.types';
--- src/shared/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Button';
-export * from './Chip';
-export * from './Checkbox';
