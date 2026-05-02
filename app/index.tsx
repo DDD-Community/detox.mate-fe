6758871b70c21ee465498d62c4eb776d3740e9bc
JiHo00 <cale.jiho@gmail.com>
2026-05-02 12:44:04 +0900

    chore : 개발용 진입점 redirect 스캐폴드 추가
    EXPO_PUBLIC_DEV_ENTRY 환경변수로 특정 화면 직접 진입을 위한
    Redirect 분기 스켈레톤(주석) 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,9 @@
+import { Redirect } from 'expo-router';
 import SplashScreen from '../src/screens/SplashScreen';
 
-export default SplashScreen;
+export default function Index() {
+  // if (__DEV__ && process.env.EXPO_PUBLIC_DEV_ENTRY) {
+  //   return <Redirect href={process.env.EXPO_PUBLIC_DEV_ENTRY as never} />;
+  // }
+  return <SplashScreen />;
+}
